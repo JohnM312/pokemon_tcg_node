40805c9cfd46a3a972c5c7479d98b3b7ba6814a3
John Mai <JohnNinja312@gmail.com>
2025-04-03 09:37:56 -0400

    Adding server side of the project

---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req, res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+let pokemon = [
+    {
+        "_id": 1,
+        "name": "Pikachu",
+        "img_name": "images/Pikachu V.jpg",
+        "type": "Electric",
+        "hp": 190,
+        "abilities": ["Lightning Blast (100+)"],
+        "rarity": "V",
+        "set": "Base Set"
+    },
+    {
+        "_id": 2,
+        "name": "Charizard",
+        "img_name": "images/Charizard EX.png",
+        "type": "Fire",
+        "hp": 180,
+        "abilities": ["Slash (60)", "Crimson Storm (200)"],
+        "rarity": "EX",
+        "set": "Base Set"
+    },
+    {
+        "_id": 3,
+        "name": "Blastoise",
+        "img_name": "images/Blastoise EX.png",
+        "type": "Water",
+        "hp": 180,
+        "abilities": ["Surf (40)", "Hydro Bazooka (100+)"],
+        "rarity": "EX",
+        "set": "Base Set"
+    },
+    {
+        "_id": 4,
+        "name": "Venusaur",
+        "img_name": "images/Venasaur EX.png",
+        "type": "Grass",
+        "hp": 190,
+        "abilities": ["Giant Bloom (100)", "Razor Leaf (60)"],
+        "rarity": "EX",
+        "set": "Base Set"
+    },
+    {
+        "_id": 5,
+        "name": "Gengar",
+        "img_name": "images/Gengar Uncommon.png",
+        "type": "Ghost",
+        "hp": 110,
+        "abilities": ["Hypnoblast (90)"],
+        "rarity": "Uncommon",
+        "set": "Haunted Shadows"
+    },
+    {
+        "_id": 6,
+        "name": "Mewtwo",
+        "img_name": "images/Mewtwo EX.png",
+        "type": "Psychic",
+        "hp": 150,
+        "abilities": ["Psychic Sphere (50)", "Psydrive (120)"],
+        "rarity": "EX",
+        "set": "Psychic Masters"
+    },
+    {
+        "_id": 7,
+        "name": "Lucario",
+        "img_name": "images/Lucario Uncommon.png",
+        "type": "Fighting",
+        "hp": 130,
+        "abilities": ["Avenging Knuckle (30+)", "Accelerating Stab (120)"],
+        "rarity": "Uncommon",
+        "set": "Battle Warriors"
+    },
+    {
+        "_id": 8,
+        "name": "Rayquaza",
+        "img_name": "images/Rayquaza EX.jpg",
+        "type": "Dragon",
+        "hp": 170,
+        "abilities": ["Intensifying Burn (10+)", "Dragon Pulse (100)"],
+        "rarity": "EX",
+        "set": "Sky Legends"
+    }
+];
+
+app.get("/api/pokemon", (req, res)=>{
+    res.send(pokemon);
+});
+
+
+app.listen(3001, ()=>{
+    console.log("I'm listening");
+});
